869af635aa0f0d9cbb69226984a2080fa93bfe6c
Riddhi Jain <rjain_be24@thapar.edu>
2026-04-05 13:31:41 +0530

    movie page UI design done

--- bms-frontend/src/pages/movies.jsx
+++ bms-frontend/src/pages/movies.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import BannerSlider from "../components/shared/BannerSlider";
+import MovieFilters from "../components/movies/MovieFilters";
+import MovieList from "../components/movies/MovieList";
+import { allMovies } from "../utils/constants";
 
 const Movies = () => {
   return (
     <div>
       <BannerSlider/>
+      <div className="flex flex-col md:flex-row bg-[#f5f5f5] min-h-screen md:px-[100px]pb-10 pt-8">
+      <MovieFilters/>
+      <MovieList allMovies={allMovies}/>
+      </div>
     </div>
   );
 }
